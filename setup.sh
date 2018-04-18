c976d18a70aae3d5061e919e2b0ef450f0a5db9a
avbravo <avbravo@gmail.com>
2018-04-18 11:30:39 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,26 +1,80 @@
-avbravo_user=$(whoami)
-avbravo_group=$(id -g -n $avbravo_user)
-avbravo_user_home=~
+#variables
+user=$(whoami)
+group=$(id -g -n $user)
+user_home=~
+dir_software=$user_home"/software"
+dir_java=$dir_software"/java"
+dir_oracle=$dir_java"/oracle"
+dir_mongodb=$dir_software"/mongodb"
+dir_netbeans=$dir_software"/netbeans"
 java_version="jdk-8u171"
+mongodb_version="jdk-8u171"
+directorioactual=$(cd "$(dirname "$0")"; pwd -P)
+search_jdk_oracle=$dir_oracle"/"$java_version"-linux-x64.tar.gz"
+search_jdk_usr="usr/local/jdk"$java_version
 
-#usuario
-#echo "usuario" $avbravo_user
-#echo "grupo" $avbravo_group
-#echo "avbravo_user_home" $avbravo_user_home
+
+#crear directorios
+echo "verificando directorios"
+if [ ! -d $dir_software ]; then
+    mkdir $dir_software
+  fi 
+ if [ ! -d $dir_java ]; then
+    mkdir $dir_java
+  fi
+ if [ ! -d $dir_oracle ]; then
+    mkdir $dir_oracle
+  fi
+
+if [ ! -d $dir_mongodb ]; then
+    mkdir $dir_mongodb
+  fi
+
+if [ ! -d $dir_netbeans ]; then
+    mkdir $dir_netbeans
+  fi
+menu = "b"
+continue="si"
+while [ "$continue" == "si" ]
+do
+  clear
+
 echo "------------------------------------------------"
 echo "             Instalador                         "
-echo "  Mercurial, git, java, mongodb, glogg editor, netbeans"
+echo "    1)Instalar (jdk$java_version ,MongoDB $mongodb_version,NetBeans $netbeans_version), mercurial, git, glogg"
+echo "   2)salir"
+echo "   Ingrese el numero de opcion"
+read menu
 echo "------------------------------------------------"
-
+if [ "$menu" == "1" ] ; then
 #mercurial
 echo "instalandpo mercurial git glogg"
 sudo apt-get install mercurial git glogg
 #------------------------------------
 #instalar java
 #------------------------------------
-echo "instalando Java jdk1.8"
+echo "instalando jdk"$java_version
+if [ ! -f $search_jdk_usr ]; then
+   echo "No se encontro el jdk" $java_version " en "$search_jdk_usr "
+   echo " Buscando version descargada en "$dir_oracle
+       if [ ! -f $search_jdk_oracle ]; then
+            echo "No existe jdk "$java_version " en "$search_jdk_oracle " se inicia la descarga desde java.oracle.com"
+            cd $dir_oracle
+            wget http://download.oracle.com/otn-pub/java/jdk/8u171-b11/512cd62ec5174c3487ac17c61aaa89e8/jdk-8u171-linux-x64.tar.gz?AuthParam=1524065010_fe30ab0fa219c2e0ef1c2c643c7c1c48
+      else
+          echo "si existe"
+       fi
+ else
+    echo "El jdk"$java_version " fue instalado anteriormente en /usr/local/jdk"$java_version
+  fi
+
 echo "descargando... jdk-: "$java_version
-wget http://download.oracle.com/otn-pub/java/jdk/8u171-b11/512cd62ec5174c3487ac17c61aaa89e8/jdk-8u171-linux-x64.tar.gz?AuthParam=1524065010_fe30ab0fa219c2e0ef1c2c643c7c1c48
+if [ ! -f $buscar ]; then
+   echo "no existe" $buscar
+ else
+ echo "si existe"
+  fi
+
 
 
 echo "descomprimiendo jdk-"$java_version  "-linux-x64.tar.gz"
@@ -66,4 +120,4 @@ sudo chmod 775 *.sh
 #------------------------------------------
 echo "Instalacion finalizada
 
-
+done
