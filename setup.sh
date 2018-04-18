7b6b870828956dc2cd2bd6b71678571b74fc8a60
avbravo <avbravo@gmail.com>
2018-04-18 12:47:23 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,9 +11,11 @@ java_version="jdk-8u171"
 mongodb_version="3.6.4"
 mongodb_dowload="https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu1604-3.6.4.tgz"
 directorioactual=$(cd "$(dirname "$0")"; pwd -P)
+#search
 search_jdk_oracle=$dir_oracle"/"$java_version"-linux-x64.tar.gz"
 search_jdk_usr="usr/local/jdk"$java_version
 search_mongodb_home=$user_home"/mongodb"
+search_mongodb_tar=$dir_mongodb"mongodb-linux-x86_64-ubuntu1604-3.6.4.tgz"
 
 
 #crear directorios
@@ -56,7 +58,7 @@ sudo apt-get install mercurial git glogg
 #instalar java
 #------------------------------------
 echo "instalando jdk"$java_version
-if [ ! -f $search_jdk_usr ]; then
+if [ ! -d $search_jdk_usr ]; then
    echo "No se encontro el jdk" $java_version " en "$search_jdk_usr "
    echo " Buscando version descargada en "$dir_oracle
        if [ ! -f $search_jdk_oracle ]; then
@@ -65,42 +67,40 @@ if [ ! -f $search_jdk_usr ]; then
             wget http://download.oracle.com/otn-pub/java/jdk/8u171-b11/512cd62ec5174c3487ac17c61aaa89e8/jdk-8u171-linux-x64.tar.gz?AuthParam=1524065010_fe30ab0fa219c2e0ef1c2c643c7c1c48
             echo "descomprimiendo jdk-"$java_version  "-linux-x64.tar.gz"
             tar xvfz jdk-8u171-linux-x64.tar.gz          
-    # else
-     #     echo "si existe"
        fi
        echo "moviendo jdk"$java_version " a "$j
        sudo mv jdk1.8.0_171 /usr/local/
+       echo "Configurando JAVA_HOME...."
+        #Java_home
+        sudo sed -i '$a export JAVA_HOME=/usr/local/jdk1.8.0_171\' /etc/profile
+        sudo sed -i '$a export JRE_HOME=${JAVA_HOME}/jre\' /etc/profile
+        sudo sed -i '$a export PATH=$PATH:${JAVA_HOME}/bin\' /etc/profile
  else
     echo "El jdk"$java_version " fue instalado anteriormente en /usr/local/jdk"$java_version
   fi
-
-echo "Configurando JAVA_HOME...."
-#Java
-sudo sed -i '$a export JAVA_HOME=/usr/local/jdk1.8.0_171\' /etc/profile
-sudo sed -i '$a export JRE_HOME=${JAVA_HOME}/jre\' /etc/profile
-sudo sed -i '$a export PATH=$PATH:${JAVA_HOME}/bin\' /etc/profile
 
 
 #------------------------------------
 #mongodb
 #--------------------------------------
 echo "Instalado Mongodb"
-if [ ! -f $search_mongodb_home ]; then
-   echo "No se encontro el " $search_mongodb_home " 
-   
-   
-#renombrar
-cd $user_home/software/mongodb
-tar xzvf mongodb-linux-x86_64-ubuntu1604-3.6.2.tgz
-cd $user_home/software/mongodb
-mv mongodb-linux-x86_64-ubuntu1604-3.6.2 $avbravo_user_home/mongodb
+if [ ! -d $search_mongodb_home ]; then
+   echo "No se encontro mongodb instalado en " $search_mongodb_home " 
+    if [ ! -f $search_mongodb_tar]; then
+       echo " Descargando mongodb"$mongodb_version " desde mongodb.com"
+       cd $dir_mongodb
+       wget $mongodb_dowload       
+       tar xzvf mongodb-linux-x86_64-ubuntu1604-3.6.4.tgz
+       mv mongodb-linux-x86_64-ubuntu1604-3.6.4 $search_mongodb_home 
+       #path
+        export PATH=$PATH:$user_home/mongodb/bin
+        sudo mkdir -p /data/db
+        sudo chmod 777 /data/db
+        echo "agregando a /etc/profile"
+        sudo sed -i '$a export PATH=$PATH:'$user_home'/mongodb/bin\' /etc/profile
+    fi
+fi
 
-#path
-export PATH=$PATH:$avbravo_user_home/mongodb/bin
-sudo mkdir -p /data/db
-sudo chmod 777 /data/db
-echo "agregando a /etc/profile"
-sudo sed -i '$a export PATH=$PATH:'$avbravo_user_home'/mongodb/bin\' /etc/profile
 
 #NetBeans
 cd $avbravo_user_home/software/netbeans
