5449a8a295bd41482732f11f19099bce73ccfe80
avbravo <avbravo@gmail.com>
2018-04-25 11:19:33 -0500

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -10,6 +10,10 @@ group=$(id -g -n $user)
 user_home=~
 DIA=`date +"%Y_%m_%d"`
 HORA=`date +"_%H_%M"`
+#-----------------------
+#limite de memoria en MB
+#-----------------------
+limite_memoria=1500
 #---------------------------------
 #Wildfly
 #---------------------------------
@@ -33,36 +37,6 @@ if [ ! -d $widlfly_dir ]; then
     echo " No esta instalado el directorio"$wildlfy_dir
 fi 
 
-#-------------------
-java_kill () {
-     killall -9 java
-}
-
-#---------------------------------------
-wildfly_restar(){
- 
- echo " Reiniciando wildfly a"$DIA $HORA
-   cd $user_home/log
-   sed -i '$a Reiniciando Wildfly $DIA $HORA' $log_name
-#-----------------------------------
-#remove .xml
-#-----------------------------------
-   cd  /opt/wildfly/standalone/data/timer-service-data
-   #obtener la cantidad de archivos
-   ls | wc -l > files.txt
-   for line in $(cat files.txt); 
-   do 
-     if [ "$line" -ge 1 ]; then
-         echo " Eliminando archivos .xml"
-         rm -r *.xml
-     else
-        echo "No hay archivos es timer-sevice-data"
-     fi
-   done
-  
-   cd $wildfly_bin_dir
-   sh ./standalone.sh -b 127.0.0.1 -bmanagement 127.0.0.1 & > salida.txt
-}
 
 #-------------------------------------------
 #log
@@ -77,68 +51,103 @@ log_file_create(){
      if [ ! -f "$user_home/log/monitor.log" ]; then
        echo "[$DIA $HORA][INFO] Creacion archivo log" >> monitor.log
      fi
-    #touch $log_name
-  
+   
 }
 
-#Agrega texto al log
-log_file_text_add(){
-$1
-    cd $user_home/log
-      if [ ! -f "$user_home/log/monitor.log" ]; then
-#   sed -i '$a mas Aqui el texto que ira en la ultima linea' filename
-    sed -i '$a $1' monitor.log
-    cd $wildfly_bin
-}
 
+
+
+#---------------------------
+#leer_memory()
+#---------------------------
 leer_memory(){
 #memoria usada
-echo " vea los archivos porcentaje.txt y memory.txt"
-log_file_text_add "Analizando memoria "$DIA $HORA
-awk '/^Mem/ {printf("%u%%", 100*$3/$2);}' <(free -m) > porcentaje.txt
-awk '/^Mem/ {print $3}' <(free -m) > memory.txt
+ echo " vea los archivos porcentaje.txt y memory.txt"
+  awk '/^Mem/ {printf("%u%%", 100*$3/$2);}' <(free -m) > porcentaje.txt
+  awk '/^Mem/ {print $3}' <(free -m) > memory.txt
 
-for line in $(cat memory.txt); 
-do 
- log_file_text_add "Memoria $line "$DIA $HORA
-
- if [ "$line" -ge 1500 ]; then
-    echo "$line  es mayor que 3000"
-    echo " ejecutar:"
-    echo " java_kill"
-    echo " wildfly_restar"
-java_kill
-wildfly_restar
- else
-    echo "$line  es menor que 3000"
- fi
- 
+ for ram in $(cat memory.txt); 
+ do 
+    sed -i '1i[$DIA $HORA][DEBUG] Memoria consumida:$ram' >> monitor.log
+    if [ "$ram" -ge $limite_memoria ]; then
+       echo "Memoria:$ram es mayor que el limite: $limite_memoria"
+       echo " ejecutar: java_kill wildfly_restar"
+       java_kill
+       wildfly_restar
+    else
+      sed -i '1i[$DIA $HORA][DEBUG] Memoria consumida:$line es menor que el limite: $limite_memoria' >> monitor.log
+      echo "$line  es menor que el limite"
+    fi
  done
 }
 
 
- clear
+#------------------------
+# java_kill()
+#-------------------
+java_kill () {
+     echo "Ejecutando killall -9 java"
+     sed -i '1i[$DIA $HORA][INFO] Ejecutando killall -9 java' >> monitor.log
+     killall -9 java
+}
 
+#---------------------------------------
+#wildfly_restar()
+#--------------------------------------
+wildfly_restar(){
+     echo " Reiniciando wildfly a"$DIA $HORA
+     sed -i '1i[$DIA $HORA][INFO] Ejecutando reinicio de wildfly' >> monitor.log
+     cd $user_home/log
+#-----------------------------------
+#remove .xml
+#-----------------------------------
+   cd  /opt/wildfly/standalone/data/timer-service-data
+   #obtener la cantidad de archivos
+   ls | wc -l > files.txt
+   for files in $(cat files.txt); 
+   do 
+     if [ "$files" -ge 1 ]; then
+         echo " Eliminando archivos .xml de standalone/data/timer-service-data"
+         sed -i '1i[$DIA $HORA][INFO] Eliminando archivos .xml de standalone/data/timer-service-data' >> monitor.log
+         rm -r *.*
+     else
+        echo "No hay archivos es timer-sevice-data"
+        sed -i '1i[$DIA $HORA][INFO] No hay archivos .xml de standalone/data/timer-service-data' >> monitor.log
+     fi
+   done
+  
+   cd $wildfly_bin_dir
+   sed -i '1i[$DIA $HORA][INFO] Reiniciando wildfly' >> monitor.log
+   sh ./standalone.sh -b 127.0.0.1 -bmanagement 127.0.0.1 & > salida.txt
+}
+
+
+
+
+#---------------------------
+# start
+#----------------------------
+
+clear
 echo "------------------------------------------------"       
 echo "        Monitor WILDFLY"
 echo "------------------------------------------------"
 
-echo " iniciando proceso de verificacion"
 log_file_create
 
-#java_kill
-
-awk '/^Mem/ {printf("%u%%", 100*$3/$2);}' <(free -m)
-echo ""
-awk '/^Mem/ {print $3}' <(free -m)
+echo " iniciando proceso de verificacion"
+sed -i '1i[$DIA $HORA][START] Iniciando proceso de verificacion' >> monitor.log
 
 
-echo "----------------------------------"
+
+#awk '/^Mem/ {printf("%u%%", 100*$3/$2);}' <(free -m)
+#echo ""
+#awk '/^Mem/ {print $3}' <(free -m)
 leer_memory
-
-    echo "--------------------------------------"
-    echo "Instalacion finalizada"
-    echo "--------------------------------------"
+ 
+ echo "--------------------------------------"
+ echo "Instalacion finalizada"
+ echo "--------------------------------------"
 
   
 
