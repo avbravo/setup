172fe56a7db382bf651dab8a6683eb114c9b53be
avbravo <avbravo@gmail.com>
2020-06-18 11:07:28 -0500

    Delete reinicar.sh

--- reinicar.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#reinicia mongodb y la aplicacion
-#Mongodb
-wget -S --spider http://localhost:27017/ 2>&1 | awk '/^  /'
-if [ $? -ne 0 ]
-then echo "Server is UP"
-else
-echo "Server is down"
-fi
